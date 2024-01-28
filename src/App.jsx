98cedd093e96dfca32390c1b6347870040d46a53
Mr. Sanjoy <--add>
2024-01-28 20:03:24 +0530

    axios working

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,58 @@
-import { useState, useContext } from 'react'
+import { useState, useContext, useEffect } from 'react'
 import './App.css'
 import { UserContext, UserContextProvider } from './context/UserContext';
 import User from './components/User';
 import { Student } from './components/Student';
 import { StudentContextProvider } from './context/StudentContext';
-
-
+import axiosInstance from './api/Product';
+import axios from 'axios'
 
 function App() {
 
   const user = useContext(UserContext);
-  console.log("User", user);
+  // console.log("User", user);
 
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    ; (async () => {
+      const response = await axiosInstance.get('/api/v1/products');
+      console.log(response.data);
+      setProduct(response?.data);
+    })();
+
+  }, [])
+
 
   return (
 
-    <div>
+    <>
+      <h1>Product</h1>
+      <div className='flex flex-wrap gap-2'>
+        {
+          product?.map((product) => {
+            return (
+              <div key={product?.id} className='mt-3'>
+                <img src={product?.images} alt="" className='w-52' />
+                <h3> T{product?.title.slice(0,20)} </h3>
+              </div>
+            )
+          })
+        }
+      </div>
+
+
+      {/* <div>
       <h1>React JS Context API App</h1>
       <User />
       <StudentContextProvider>
         <Student />
       </StudentContextProvider>
-    </div>
+    </div> */}
+
+
+    </>
 
   )
 }
---
+++ src/api/Product.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+    baseURL: 'https://api.escuelajs.co/',
+    timeout: 5000,
+    headers: { 'X-Custom-Header': 'foobar' }
+
+});
+
+export default axiosInstance;
