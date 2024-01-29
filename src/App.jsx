db47f963c5783a8364f4da561fa0a82e70bc121d
Mr. Sanjoy <--add>
2024-01-29 09:56:00 +0530

    image is showing

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,10 @@ import User from './components/User';
 import { Student } from './components/Student';
 import { StudentContextProvider } from './context/StudentContext';
 import axiosInstance from './api/Product';
-import axios from 'axios'
 import Cart from './components/Cart';
 import CartContext, { CartContextProvider } from './context/CartContext';
-import Parent from './components/Parent';
-import Childe from './components/Child';
+import Skeleton from '@mui/material/Skeleton';
+import noImage from '../public/no_image.jpg'
 
 function App() {
 
@@ -37,22 +36,32 @@ function App() {
     cartItems.setCartItem(cartItems.cartItem + 1)
     console.log("After Item :", cartItems.cartItem);
   }
+  const skeleton = <Skeleton
+    sx={{ bgcolor: 'grey.900' }}
+    variant="rectangular"
+    width={210}
+    height={118}
+  />
 
   return (
 
 
     <>
       <Cart />
-      <Parent >
-        <Childe />
-      </Parent>
+      <Skeleton
+        sx={{ bgcolor: 'red' }}
+        variant="rectangular"
+        width={210}
+        height={118}
+      />
+
       <h1>Product</h1>
       <div className='flex flex-wrap gap-2'>
         {
           product?.map((product, index) => {
             return (
               <div key={index} className='mt-3'>
-                <img src={product?.images} alt={product?.title.slice(0, 10)} className='w-52' />
+                <img src={product?.images.length === 3 ? product?.images : noImage} alt={''} className='w-52' />
                 <h3>{product?.title.slice(0, 20)} </h3>
                 <h3>{product?.price} </h3>
                 <h3>{product?.category?.name} </h3>
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,11 @@ import App from './App.jsx'
 import './index.css'
 import { UserContextProvider } from './context/UserContext.jsx'
 import { CartContextProvider } from './context/CartContext.jsx'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
